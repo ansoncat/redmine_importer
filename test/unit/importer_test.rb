d40a6ff68d8021c907d987e0cd9eb9f94c8a76a1
Greg Mefford <gregory.mefford@gmail.com>
2010-11-12 21:15:19 -0500

    Refactored test code to be less repetitive.

--- test/unit/importer_test.rb
+++ test/unit/importer_test.rb
@@ -6,71 +6,61 @@ class ImporterTest < ActiveSupport::TestCase
   include ActionController::TestProcess
   
   def setup
-    @project      ||= Project.generate!(:name => 'Test Project', :identifier => 'testproject')
+    @project      ||= Project.generate!(
+                        :name => 'Test Project',
+                        :identifier => 'testproject')
     @request        = ActionController::TestRequest.new
     @response       = ActionController::TestResponse.new
     @controller     = ImporterController.new
     @fixture_file ||= fixture_file_upload("/test_issues.csv", "text/csv")
-  end
-  
-  def upload_file
-    post :match,
+    @post_options   = {
       :project_id => @project.id,
       :file => @fixture_file,
       :splitter => ",",
       :wrapper => "\"",
       :encoding => "U"
+    }
   end
   
   context "#match" do
     
     should "accept file uploads" do
-      upload_file
+      post :match, @post_options
       assert_equal "200 OK", @response.status
     end
 
     should "not error when missing project_id" do
-      post :match,
-        :file => @fixture_file,
-        :splitter => ",",
-        :wrapper => "\"",
-        :encoding => "U"
+      options = @post_options.dup
+      options.delete(:project_id)
+      post :match, options
       assert_equal "200 OK", @response.status
     end
 
     should "not error when missing file" do
-      post :match,
-        :project_id => @project.id,
-        :splitter => ",",
-        :wrapper => "\"",
-        :encoding => "U"
+      options = @post_options.dup
+      options.delete(:file)
+      post :match, options
       assert_equal "200 OK", @response.status
     end
 
     should "not error when missing splitter" do
-      post :match,
-        :project_id => @project.id,
-        :file => @fixture_file,
-        :wrapper => "\"",
-        :encoding => "U"
+      options = @post_options.dup
+      options.delete(:splitter)
+      post :match, options
       assert_equal "200 OK", @response.status
     end
 
     should "not error when missing wrapper" do
-      post :match,
-        :project_id => @project.id,
-        :file => @fixture_file,
-        :splitter => ",",
-        :encoding => "U"
+      options = @post_options.dup
+      options.delete(:wrapper)
+      post :match, options
       assert_equal "200 OK", @response.status
     end
 
     should "not error when missing encoding" do
-      post :match,
-        :project_id => @project.id,
-        :file => @fixture_file,
-        :splitter => ",",
-        :wrapper => "\""
+      options = @post_options.dup
+      options.delete(:wrapper)
+      post :match, options
       assert_equal "200 OK", @response.status
     end
 
